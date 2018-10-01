ddc26243c0822f30dce6d13bca4353a9127113c6
Christian Bonami <cbonami@gmail.com>
2018-10-01 17:48:43 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Shop Gateway
+
+## Links
+
+* [Spring Cloud Gateway - Baeldung](https://www.baeldung.com/spring-cloud-gateway)
+* [Spring Cloud Gateway - Routing Example](https://stackoverflow.com/questions/48865174/spring-cloud-gateway-proxy-forward-the-entire-sub-part-of-url)
---
+++ src/main/java/be/vdab/demo/gateway/GatewayApplication.java
@@ -0,0 +1,37 @@
+package be.vdab.demo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+//    @Bean
+//    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(r ->
+//                        r.path("/productcatalogue/**")
+//                                .filters(
+//                                        f -> f.stripPrefix(1)
+//                                )
+//                                .uri("http://productcatalogue:8080/")
+//                )
+//                .route(r ->
+//                        r.path("/stockmanager/**")
+//                                .filters(
+//                                        f -> f.stripPrefix(1)
+//                                )
+//                                .uri("http://stockmanager:8080/")
+//                )
+//                .build();
+//    }
+
+}
