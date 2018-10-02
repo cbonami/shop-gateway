adef76eb80b945b5df6023e584e8882206314f00
Christian Bonami <cbonami@gmail.com>
2018-10-02 18:13:08 +0200

    gcloud config set container/use_client_certificate True

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,19 @@ if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     gcloud --quiet components update kubectl
     echo "step 2"
     # Push to Google container registry
-    docker build -t eu.gcr.io/$CLOUDSDK_CORE_PROJECT/$MICROSERVICE_NAME:v1 .
+    docker build -t gcr.io/$CLOUDSDK_CORE_PROJECT/$MICROSERVICE_NAME:v1 .
     echo "step 3"
     #gcloud components install docker-credential-gcloud
     #echo "step 3b"
     #docker-credential-gcloud configure-docker
+    #gcloud auth configure-docker
     echo "step 4"
-    docker push eu.gcr.io/$CLOUDSDK_CORE_PROJECT/$MICROSERVICE_NAME:v1
-    echo "step 5"
-    # Deploy to the cluster
+    gcloud config set container/use_client_certificate True
     gcloud container clusters get-credentials $CLOUDSDK_CORE_PROJECT
+    echo "step 5"
+    docker push gcr.io/$CLOUDSDK_CORE_PROJECT/$MICROSERVICE_NAME:v1
     echo "step 6"
+    # Deploy to the cluster
     #  kubectl apply -f config/k8s/mongo.yml
     kubectl apply -f config/gcp/$MICROSERVICE_NAME.yml
     echo "step 7"
