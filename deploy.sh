34ef8c8ff9029c62c6888731b4c5143a417cc001
Christian Bonami <cbonami@gmail.com>
2018-10-02 16:49:25 +0200

    GOOGLE_APPLICATION_CREDENTIALS

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    echo "step 0"
+    pwd
     echo "step 1"
     gcloud --quiet components update kubectl
     echo "step 2"
     # Push to Google container registry
     docker build -t eu.gcr.io/$CLOUDSDK_CORE_PROJECT/$MICROSERVICE_NAME:v1 .
     echo "step 3"
-    gcloud components install docker-credential-gcloud
-    echo "step 3b"
-    docker-credential-gcloud configure-docker
-    echo "step 3c"
-    docker push eu.gcr.io/$CLOUDSDK_CORE_PROJECT/$MICROSERVICE_NAME:v1
+    #gcloud components install docker-credential-gcloud
+    #echo "step 3b"
+    #docker-credential-gcloud configure-docker
     echo "step 4"
+    docker push eu.gcr.io/$CLOUDSDK_CORE_PROJECT/$MICROSERVICE_NAME:v1
+    echo "step 5"
     # Deploy to the cluster
     gcloud container clusters get-credentials $CLOUDSDK_CORE_PROJECT
-    echo "step 5"
+    echo "step 6"
     #  kubectl apply -f config/k8s/mongo.yml
     kubectl apply -f config/gcp/$MICROSERVICE_NAME.yml
-    echo "step 6"
+    echo "step 7"
     kubectl rolling-update web-controller --image=gcr.io/$CLOUDSDK_CORE_PROJECT/$MICROSERVICE_NAME:v1 --image-pull-policy Always
     echo "done !"
 fi
