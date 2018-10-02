b1cff5eb9e2c337d22e7653598c0e075b55660e6
Christian Bonami <cbonami@gmail.com>
2018-10-02 22:19:25 +0200

    fixed missing file

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     docker build -t eu.gcr.io/$CLOUDSDK_CORE_PROJECT/$MICROSERVICE_NAME:v1 .
     gcloud auth configure-docker
     # next line is not needed imo
-    docker login -u _json_key -p "$(cat gcloud.json)" https://gcr.io
+    # docker login -u _json_key -p "$(cat gcloud.json)" https://gcr.io
     docker push eu.gcr.io/$CLOUDSDK_CORE_PROJECT/$MICROSERVICE_NAME:v1
     # Deploy to the cluster
     # https://github.com/kubernetes/kubernetes/issues/28612
@@ -13,7 +13,7 @@ if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     gcloud container clusters get-credentials shop
     echo "step 5"
     #  kubectl apply -f config/k8s/mongo.yml
-    kubectl apply -f config/k8s/$MICROSERVICE_NAME.yml
+    kubectl apply -f k8s/$MICROSERVICE_NAME.yml
     echo "step 6"
     kubectl rolling-update web-controller --image=eu.gcr.io/$CLOUDSDK_CORE_PROJECT/$MICROSERVICE_NAME:v1 --image-pull-policy Always
     echo "done !"
